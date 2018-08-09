0b7913f7e0b146eca8baf4f17ca2f9cf0d0cc6bc
abraham.macias <abraham.macias@softtek.com>
2018-08-09 12:25:41 -0500

    Primer cmmit

---
+++ src/Test.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+object Test {
+  def main(args: Array[String])
+  {
+    val conf:SparkConf = new SparkConf().setAppName("Histogram").setMaster("local")
+		val sc:SparkContext = new SparkContext(conf)
+		
+    val rdd = sc.parallelize(0 to 5)
+    
+    println("tamaño del rdd: " + rdd.count() + " El numero de particiones: " + rdd.getNumPartitions)
+    rdd.collect().foreach(println)
+    
+  }
+}
